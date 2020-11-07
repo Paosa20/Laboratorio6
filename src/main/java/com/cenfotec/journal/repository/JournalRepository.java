1c3abbd46d55683cb51f58eea866f52c6631d26a
Paosa20 <msanchezc1@ucenfotec.ac.cr>
2020-11-07 16:43:25 -0600

    lab06

---
+++ src/main/java/com/cenfotec/journal/repository/JournalRepository.java
@@ -0,0 +1,9 @@
+package com.cenfotec.journal.repository;
+
+
+import com.cenfotec.journal.domain.Journal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JournalRepository extends JpaRepository<Journal,
+        Long> {
+}
